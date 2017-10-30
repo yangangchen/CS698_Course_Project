2eff698b926b9b8fed8de87af111f844809c1ae6
yangangchen <yangangchen.0@gmail.com>
2017-10-30 13:56:59 -0400

    CS 698 Course Project, Yangang Chen, April 15, 2017. GPL License added.

--- gitupload.sh
+++ gitupload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git init
-git remote add origin https://github.com/yangangchen/CS698_Course_Project.git
+#git init
+#git remote add origin https://github.com/yangangchen/CS698_Course_Project.git
 git remote show origin
 git add .
 git commit -m 'CS 698 Course Project, Yangang Chen, April 15, 2017. GPL License added.'
